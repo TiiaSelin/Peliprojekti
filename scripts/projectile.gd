18a1fbc734c8cae32fd6ac6aa8101a9916e40302
ExcavatorZ <excavatorz@hotmail.fi>
2025-01-30 13:00:29 +0200

    Limited projectile lifespan.

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -5,13 +5,13 @@ var travel_distance = 0
 func _physics_process(delta: float) -> void:
 	var direction = Vector2.RIGHT.rotated(rotation)
 	const projectile_speed = 600
-	#const range = 900
+	const range = 900
 	position += direction * projectile_speed * delta
 	
-	#travel_distance += projectile_speed * delta
-	#if travel_distance > range:
-		#queue_free()
-		#travel_distance = 0
+	travel_distance += projectile_speed * delta
+	if travel_distance > range:
+		queue_free()
+		travel_distance = 0
 
 '''
 func _on_body_entered(body: Node2D) -> void:
