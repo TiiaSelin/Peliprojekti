4ca993800a82279aa0e4234143cd4d8d0a58f9cb
ExcavatorZ <excavatorz@hotmail.fi>
2025-01-30 14:21:18 +0200

    Added health property and function.

--- scripts/mob.gd
+++ scripts/mob.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var player = get_node("/root/Game/Player")
 
-
+var health = 1
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
@@ -10,3 +10,9 @@ func _physics_process(delta: float) -> void:
 	#määritellään vihollisten nopeus
 	velocity = direction * 300.0
 	move_and_slide()
+
+func take_damage():
+	health -= 1
+	
+	if health == 0:
+		queue_free()
--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -15,5 +15,7 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
+	if body.has_method("take_damage"):
+		body.take_damage()
 	queue_free()
 	
