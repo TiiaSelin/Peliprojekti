0d74cf43b7482df1e96904055f4a2e8844d70f74
velu <vertti.koivula@gmail.com>
2025-02-13 12:47:54 +0200

    Koitan saada vihollisen kuolema-äänen toimimaan. Check.

--- scripts/mob.gd
+++ scripts/mob.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @onready var player = get_node("/root/Game/Player")
+@onready var vihollinenKuolema = $vihollinenKuolema
+
 
 var health = 1
 
@@ -13,6 +15,8 @@ func _physics_process(delta: float) -> void:
 
 func take_damage():
 	health -= 1
+	vihollinenKuolema.play() # Vihollisen kuolema-ääni
 	
 	if health == 0:
+		await vihollinenKuolema.finished  # Wait for the "finished" signal
 		queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -12,7 +12,6 @@ signal health_depleted
 var health = 100.0
 	
 
-
 # Pelaajahahmon liike
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
