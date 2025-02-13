044596a59a18a700f66d1d34169cd91cf24a862e
velu <vertti.koivula@gmail.com>
2025-02-13 11:27:24 +0200

    Lisätty low health-ääni

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
 @onready var taustamusiikki = $taustaMusiikki
+@onready var kuolemisAani = $pelaajaKuolema
+@onready var pelaajaOsuma = $pelaajaOsuma
+@onready var lowHealth = $lowHealth
+
 
 signal health_depleted 
 
@@ -15,17 +19,34 @@ func _physics_process(delta: float) -> void:
 	# Hahmon nopeus
 	velocity = direction * 650
 	move_and_slide()
+	playLowHealthWarning() # Low health-varoitusääni kun health alle 30
+	
+
 
 #Pelaajan ottama damagen määrittelyä
 	const DAMAGE_RATE = 5.0
 	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
 	if overlapping_mobs.size() > 0:
 		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		# Audio
+		if not pelaajaOsuma.playing:
+			pelaajaOsuma.play()
+		
+		
 		%ProgressBar.value = health
 		if health <= 0:
 			health_depleted.emit()
+			# Audio
+			kuolemisAani.play()
 			
 	
+	
+	
+	
+# Audiofunktiot
+func playLowHealthWarning():
+	if health <= 30 and not lowHealth.playing:
+		lowHealth.play()
 # Musiikintoisto kentän alkaessa
 func soitaMusiikki():
 	taustamusiikki.play()
