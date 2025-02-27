41ddca59369a4a4dff8b5a6ee821485137508ed4
velu <vertti.koivula@gmail.com>
2025-02-27 13:04:57 +0200

    Lisätty final level-tason aloitusääni sekä final level-tason musiikki kansioon

--- scripts/final_level.gd
+++ scripts/final_level.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var final_timer: Timer = $final_timer
 @onready var final_level: CanvasLayer = $FinalLevel
+@onready var finalLevelÄäni = $finalLevelÄäni
+
 
 func _on_player_health_depleted() -> void:
 	%GameOver.visible = true
@@ -12,6 +14,8 @@ func _ready():
 	get_tree().paused = true
 	final_timer.process_mode = Node.PROCESS_MODE_ALWAYS
 	final_timer.start()
+	finalLevelÄäni.play() # Soita pahaenteinen ääni ennen bossia
+	
 
 func spawn_mob():
 	var new_mob = preload("res://scenes/big_bee.tscn").instantiate()
