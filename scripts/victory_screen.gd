6aed9dd3870132cb0fcba684f8dff15cdc315227
TiiaSelin <tiiaselin92@gmail.com>
2025-02-28 18:58:47 +0200

    new game nappi aloittaa uuden pelin

--- scripts/victory_screen.gd
+++ scripts/victory_screen.gd
@@ -2,12 +2,19 @@ extends Control
 
 @onready var clickSound = $clickSound
 
+
+
 func _ready() -> void:
 	$GridContainer/quit.connect("mouse_entered", _on_button_hover)
 
+
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-
+	
+func _on_newgame_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+	
 func _on_button_hover() -> void:
 	clickSound.play()
 
